dd096b1130ae8253bdb563e9a98dcd985e831062
1McFly10 <vlados3702@gmail.com>
2021-04-29 12:42:18 +0300

    header ready

---
+++ scripts/title.js
@@ -0,0 +1,10 @@
+let typed = new Typed('.header-banner-text', {
+    strings: ["бизнеса", 'сайта', 'продвижения '],
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 500,
+    typeSpeed:35,
+    backSpeed:25,
+    backDelay:2000,
+    loop:true,
+    contentType:"html"
+});
